995972e31eb1c8281c7eec880709bc271bf0233c
mgrist <matthewgrist0311@gmail.com>
2024-04-18 22:44:29 -0500

    Refactor elim script, factor in cuts

--- elimination.js
+++ elimination.js
@@ -9,16 +9,79 @@ elim 5
 ############################################################
 */
 
-var data = {};
-var playerCount = null;
+var raceinfo = null;
+var g_steps_per_second = 128;
+var g_step_size = 1.0 / g_steps_per_second;
+var g_holeshot_index = 1;
 
-function timingLog(slot, pos, time) {
-  if (!data[slot]) {
-    data[slot] = [];
+function elimPlayers(slot, cmdline) {
+  try {
+    if (cmdline.match(/^\s*elim\b/) == null) {
+      return command_handler(slot, cmdline);
+    }
+
+    if (!isAdmin(slot)) {
+      mxserver.send(slot, "You do not have permission to use this command");
+      return 1;
+    }
+
+    var cliArgs = cmdline.match(/^\s*elim\s*(\d+)/);
+    var resultsFile = mxserver.file_to_string("./results.txt").split("\n");
+    raceinfo = parse_server_results(resultsFile);
+    sanitize_raceinfo(raceinfo);
+
+    var playerCount = raceinfo.players.length;
+
+    // Error check the users input
+    if (cliArgs == null) {
+      mxserver.send(slot, "Usage: server, elim <number>");
+      return 1;
+    } else if (cliArgs.length < 2) {
+      mxserver.send(slot, "Usage: server, elim <number>");
+      return 1;
+    } else if (cliArgs[1] < 1) {
+      mxserver.send(slot, "Invalid number");
+      return 1;
+    } else if (cliArgs[1] > mxserver.max_slots) {
+      mxserver.send(
+        slot,
+        "Number too high. Maximum slots is " + mxserver.max_slots
+      );
+      return 1;
+    } else if (playerCount < cliArgs[1]) {
+      mxserver.send(
+        slot,
+        "Not enough players to eliminate. Current player count is " +
+          playerCount
+      );
+      return 1;
+    }
+
+    var numberToElim = parseInt(cliArgs[1]);
+    mxserver.log("Eliminating " + numberToElim + " players\n");
+    for (var num = 1; num <= numberToElim; num++) {
+      var orderCount = raceinfo.order.legnth;
+      var playerIndex = raceinfo.order[orderCount - num].index;
+      var playerInfo = raceinfo.players[playerIndex];
+      var message = "[red]" + playerInfo.name + " has been eliminated.";
+      mxserver.broadcast(
+        convert_to_ansi(message) + convert_to_ansi("[normal]")
+      );
+      mxserver.schedule_command("at +0 forcespec " + playerInfo.uid);
+    }
+  } catch (e) {
+    dprint(e);
   }
-  data[slot].push({ pos, time });
-  timing_handler(slot, pos, time);
+
+  return 1;
 }
+
+var command_handler = mxserver.command_handler;
+mxserver.command_handler = elimPlayers;
+
+///////////////////////
+// UTILITY FUNCTIONS //
+///////////////////////
 
 function isAdmin(slot) {
   var rank = mxserver.get_rank(slot);
@@ -26,102 +89,6 @@ function isAdmin(slot) {
     return true;
   }
   return false;
-}
-
-function getEliminatedPlayers(n) {
-  // Convert the data object's properties to an array for easier manipulation
-  var playerEntries = [];
-  for (var playerSlot in data) {
-    var slotData = data[playerSlot];
-    var lastEntryTime = slotData[slotData.length - 1].time;
-    playerEntries.push({
-      slot: playerSlot,
-      gateCount: slotData.length,
-      lastTime: lastEntryTime
-    });
-  }
-
-  // Sort by the number of gates passed in ascending order, and by last gate time in descending order
-  playerEntries.sort(function (a, b) {
-    if (a.gateCount === b.gateCount) {
-      return b.lastTime - a.lastTime; // Higher time is worse, so sort descending here
-    }
-    return a.gateCount - b.gateCount; // Fewer gates is worse, so sort ascending
-  });
-
-  // Get the slots of the first 'n' entries (these are the players to be eliminated)
-  var eliminatedPlayers = [];
-  for (var i = 0; i < n; i++) {
-    if (i < playerEntries.length) {
-      eliminatedPlayers.push(playerEntries[i].slot);
-    }
-  }
-
-  return eliminatedPlayers;
-}
-
-function getPlayerCount() {
-  var status;
-  var num = 0;
-
-  for (i = 0; i < mxserver.max_slots; i++) {
-    status = mxserver.get_status(i);
-    if (status == "Player") {
-      num++;
-    }
-  }
-
-  return num;
-}
-
-function elimPlayers(slot, cmdline) {
-  if (cmdline.match(/^\s*elim\b/) == null) {
-    return command_handler(slot, cmdline);
-  }
-
-  if (!isAdmin(slot)) {
-    mxserver.send(slot, "You do not have permission to use this command");
-    return 1;
-  }
-
-  var cliArgs = cmdline.match(/^\s*elim\s*(\d+)/);
-  var playerCount = getPlayerCount();
-
-  // Error check the users input
-  if (cliArgs == null) {
-    mxserver.send(slot, "Usage: server, elim <number>");
-    return 1;
-  } else if (cliArgs.length < 2) {
-    mxserver.send(slot, "Usage: server, elim <number>");
-    return 1;
-  } else if (cliArgs[1] < 1) {
-    mxserver.send(slot, "Invalid number");
-    return 1;
-  } else if (cliArgs[1] > mxserver.max_slots) {
-    mxserver.send(
-      slot,
-      "Number too high. Maximum slots is " + mxserver.max_slots
-    );
-    return 1;
-  } else if (playerCount < cliArgs[1]) {
-    mxserver.send(
-      slot,
-      "Not enough players to eliminate. Current player count is " + playerCount
-    );
-    return 1;
-  }
-
-  var numberToElim = parseInt(cliArgs[1]);
-  mxserver.log("Eliminating " + numberToElim + " players\n");
-  var eliminatedPlayers = getEliminatedPlayers(numberToElim);
-  for (var num = 0; num < eliminatedPlayers.length; num++) {
-    var playerInfo = mxserver.get_slot_info(parseInt(eliminatedPlayers[num]));
-    var message = "[red]" + playerInfo.name + " has been eliminated.";
-    mxserver.broadcast(convert_to_ansi(message) + convert_to_ansi("[normal]"));
-    mxserver.schedule_command("at +0 forcespec " + playerInfo.uid);
-  }
-
-  return 1;
 }
 
 function convert_to_ansi(s) {
@@ -144,16 +111,464 @@ function convert_to_ansi(s) {
   });
 }
 
-function finishLog() {
-  data = {};
-  finish_handler();
+function dprint(s) {
+  document.write(s + "\n");
 }
 
-var timing_handler = mxserver.timing_handler;
-mxserver.timing_handler = timingLog;
+function htmlquote(s) {
+  return String(s)
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;");
+}
 
-var command_handler = mxserver.command_handler;
-mxserver.command_handler = elimPlayers;
+// escape alphanumeric and _
+function escape_string(s) {
+  var c, e, i, n;
 
-var finish_handler = mxserver.finish_handler;
-mxserver.finish_handler = finishLog;
+  n = s.length;
+
+  e = "";
+
+  for (i = 0; i < n; i++) {
+    c = s.charAt(i);
+    if (c.search(/[^A-Za-z0-9_]/) == -1) e += c;
+    else e += "$" + c.charCodeAt(0).toString() + "$";
+  }
+
+  return e;
+}
+
+function get_var(s) {
+  var i;
+
+  i = s.indexOf("=");
+
+  if (i >= 0) return { name: s.substring(0, i), value: s.substring(i + 1) };
+
+  return null;
+}
+
+function position_to_lap(raceinfo, p) {
+  if (p < raceinfo.firstlap) return 0;
+
+  return 1 + Math.floor((p - raceinfo.firstlap) / raceinfo.normallap);
+}
+
+function lap_to_timeidx(raceinfo, lap) {
+  if (lap == 0) return 0;
+
+  return raceinfo.firstlap + raceinfo.normallap * (lap - 1) - 1;
+}
+
+function wrap_timing_position(raceinfo, i) {
+  if (i < raceinfo.firstlap) return i;
+
+  return raceinfo.firstlap + ((i - raceinfo.firstlap) % raceinfo.normallap);
+}
+
+function get_section_time(times, i0, i1) {
+  var i, t0, t1;
+
+  for (i = i0 < 0 ? 0 : i0; i <= i1; i++) if (!(i in times)) return -1;
+
+  t0 = i0 < 0 ? 0 : times[i0];
+  t1 = times[i1];
+
+  return t1 - t0;
+}
+
+function get_average_section_time(raceinfo, times, i0, i1) {
+  var i, n, t, s, p, fll, nll;
+
+  p = times.length;
+  fll = raceinfo.firstlap;
+  nll = raceinfo.normallap;
+
+  if (i0 < fll) return get_section_time(times, i0, i1);
+
+  t = 0;
+  n = 0;
+  i = wrap_timing_position(raceinfo, i0);
+
+  for (i = wrap_timing_position(raceinfo, i0); i + (i1 - i0) < p; i += nll) {
+    s = get_section_time(times, i, i + (i1 - i0));
+
+    if (s >= 0) {
+      t += s;
+      n++;
+    }
+  }
+
+  if (n == 0) return -1;
+
+  return Math.floor(t / n);
+}
+
+function get_baseline_section_time(raceinfo, index, i0, i1) {
+  var i, s, t, n, times;
+
+  t = 0;
+  n = 0;
+
+  for (i in raceinfo.players) {
+    times = raceinfo.players[i].times;
+
+    s = get_average_section_time(raceinfo, times, i0, i1);
+
+    if (s >= 0) {
+      t += s;
+      n++;
+    }
+  }
+
+  if (n == 0) return g_steps_per_second;
+
+  return Math.floor(t / n);
+}
+
+function get_cutting_penalty(raceinfo, index) {
+  var times, i, n, r, p, t, t1, t0;
+
+  times = raceinfo.players[index].times;
+  n = times.length;
+  p = 0;
+  r = 0;
+  t0 = 0;
+
+  for (i = 0; i < n; i++) {
+    if (!(i in times)) r++;
+    else {
+      t1 = times[i];
+      if (r > 0) {
+        t = get_baseline_section_time(raceinfo, index, i - r - 1, i);
+
+        if (t + g_steps_per_second > t1 - t0)
+          p += t + g_steps_per_second - (t1 - t0);
+
+        r = 0;
+      }
+      t0 = t1;
+    }
+  }
+
+  return p;
+}
+
+function get_lap_time(raceinfo, times, lap) {
+  var lastlap, lapstart, lapend;
+
+  lastlap = position_to_lap(raceinfo, times.length);
+
+  if (lap < 0) lap = lastlap;
+
+  if (lap < 1 || lap > lastlap) return 0;
+
+  lapstart = lap_to_timeidx(raceinfo, lap - 1);
+  lapend = lap_to_timeidx(raceinfo, lap);
+
+  if (!(lapstart in times && lapend in times)) return 0;
+
+  if (lap == 1) lapstart = raceinfo.starttime;
+  else lapstart = times[lapstart];
+  lapend = times[lapend];
+
+  return lapend - lapstart;
+}
+
+function lap_no_cuts(raceinfo, times, lap) {
+  var i, lapstart, lapend;
+
+  lapstart = lap_to_timeidx(raceinfo, lap - 1);
+  lapend = lap_to_timeidx(raceinfo, lap);
+
+  for (i = lapstart; i <= lapend; i++) if (!(i in times)) return 0;
+
+  return 1;
+}
+
+function best_lap(raceinfo, times, nocuts) {
+  var i, j, n, best, t0, t1;
+
+  n = position_to_lap(raceinfo, times.length);
+
+  if (n < 2) return 0;
+
+  best = 2;
+
+  for (i = 3; i <= n; i++) {
+    t0 = get_lap_time(raceinfo, times, best);
+    t1 = get_lap_time(raceinfo, times, i);
+
+    if (nocuts) {
+      if (!lap_no_cuts(raceinfo, times, i)) continue;
+      if (!lap_no_cuts(raceinfo, times, best)) t0 = 0.0;
+    }
+
+    if (t0 == 0.0 || (t1 != 0.0 && t1 < t0)) best = i;
+  }
+
+  if (nocuts && !lap_no_cuts(raceinfo, times, best)) return 0;
+
+  return best;
+}
+
+function best_time_to_lap(raceinfo, lap) {
+  var i, ti, t, p, btime;
+
+  btime = 0;
+
+  ti = lap_to_timeidx(raceinfo, lap);
+
+  for (i in raceinfo.players) {
+    p = raceinfo.players[i];
+
+    if (!(ti in p.times)) continue;
+
+    t = p.times[ti];
+
+    if (btime == 0 || t < btime) btime = t;
+  }
+
+  return btime;
+}
+
+function first_checkered_time(raceinfo) {
+  var i, p, t, l, btime, laps;
+
+  btime = 0;
+  laps = 0;
+
+  if (raceinfo.time > 0) {
+    for (laps = 1; ; laps++) {
+      t = best_time_to_lap(raceinfo, laps);
+
+      if (t == 0) return 0;
+      else if (t >= raceinfo.time + raceinfo.starttime) break;
+    }
+  }
+
+  laps += raceinfo.laps;
+
+  for (i in raceinfo.players) {
+    p = raceinfo.players[i];
+
+    l = position_to_lap(raceinfo, p.times.length);
+
+    if (l < laps) continue;
+
+    t = p.times[lap_to_timeidx(raceinfo, laps)];
+
+    if (btime == 0 || t < btime) btime = t;
+  }
+
+  return btime;
+}
+
+function remove_lagger_bonus_laps(raceinfo) {
+  var i, p, l, n, fct, t;
+
+  fct = raceinfo.firstcheckeredtime;
+
+  if (fct == 0) return;
+
+  for (i in raceinfo.players) {
+    p = raceinfo.players[i];
+    n = position_to_lap(raceinfo, p.times.length);
+    for (l = 1; l <= n; l++) {
+      t = lap_to_timeidx(raceinfo, l);
+      if (p.times[t] > fct && p.times.length > t + 1)
+        p.times.splice(t + 1, p.times.length - (t + 1));
+    }
+  }
+}
+
+function compare_position_and_time(a, b) {
+  if (a.position == b.position) return a.time - b.time;
+  return -(a.position - b.position);
+}
+
+function update_running_order(raceinfo) {
+  var i, n, order, p, pos, time, penalty, bl;
+
+  n = 0;
+  order = [];
+
+  raceinfo.firstcheckeredtime = first_checkered_time(raceinfo);
+
+  remove_lagger_bonus_laps(raceinfo);
+
+  for (i in raceinfo.players) {
+    p = raceinfo.players[i];
+
+    pos = p.times.length;
+
+    pos = lap_to_timeidx(raceinfo, position_to_lap(raceinfo, pos)) + 1;
+
+    if (pos <= 1) time = raceinfo.starttime;
+    else time = p.times[pos - 1];
+
+    penalty = get_cutting_penalty(raceinfo, i);
+
+    bl = best_lap(raceinfo, p.times, false);
+
+    if (bl >= 2) {
+      bl = get_lap_time(raceinfo, p.times, bl);
+
+      while (penalty > bl && pos > raceinfo.firstlap) {
+        penalty -= bl;
+        pos -= raceinfo.normallap;
+      }
+    }
+
+    time += penalty;
+
+    order[n] = { index: i, position: pos, time: time };
+    n++;
+  }
+
+  order.sort(compare_position_and_time);
+
+  raceinfo.order = order;
+
+  for (i in raceinfo.order) raceinfo.players[raceinfo.order[i].index].order = i;
+}
+
+function get_position(raceinfo, i, ti) {
+  var t, t2, p, x;
+
+  if (i in raceinfo.players && ti in raceinfo.players[i].times)
+    t = raceinfo.players[i].times[ti];
+  else t = 1000000000.0;
+
+  p = 1;
+
+  for (x in raceinfo.players) {
+    x = raceinfo.players[x];
+    if (ti in x.times) t2 = x.times[ti];
+    else t2 = 1000000000.0;
+    if (t2 >= 0.0 && t2 < t) p = p + 1;
+  }
+
+  return p;
+}
+
+function require_and_quote_string(obj, s, def) {
+  if (!(s in obj)) obj[s] = def;
+
+  obj[s] = htmlquote(obj[s]);
+}
+
+function sanitize_raceinfo(raceinfo) {
+  var i, n, p, requiredints;
+
+  if (!("time" in raceinfo)) raceinfo.time = 0;
+
+  requiredints = ["firstlap", "normallap", "laps", "starttime", "time", "date"];
+
+  for (i in requiredints) {
+    i = requiredints[i];
+
+    if (!(i in raceinfo)) throw 'Missing required variable "' + i + '"';
+
+    n = parseInt(raceinfo[i]);
+
+    if (isNaN(n)) throw '"' + i + '" is not a valid integer';
+
+    raceinfo[i] = n;
+  }
+
+  require_and_quote_string(raceinfo, "longname", raceinfo.dir);
+
+  if (!("dir" in raceinfo)) raceinfo.dir = "unknown";
+
+  raceinfo.dir = escape_string(raceinfo.dir);
+
+  for (p in raceinfo.players) {
+    p = raceinfo.players[p];
+
+    require_and_quote_string(p, "name", "Mystery Rider");
+    require_and_quote_string(p, "bike", "???");
+    require_and_quote_string(p, "number", "???");
+    require_and_quote_string(p, "uid", "0");
+  }
+}
+
+function parse_server_results(lines) {
+  var readvars = 0,
+    readplayers = 1,
+    readtimes = 2;
+  var l, s, t, v, slot, raceinfo, players, stage, timeidx, time;
+  var requiredints = ["firstlap", "normallap", "time", "laps", "starttime"];
+
+  raceinfo = {};
+  players = [];
+
+  stage = readvars;
+
+  raceinfo.time = "0";
+
+  for (l in lines) {
+    s = lines[l];
+
+    if (!s) break;
+
+    if (stage == readvars) {
+      if (s == "players:") {
+        stage = readplayers;
+      } else {
+        v = get_var(s);
+
+        if (v) raceinfo[v.name] = v.value;
+      }
+    } else if (stage == readplayers) {
+      if (s == "times:") {
+        stage = readtimes;
+      } else {
+        v = get_var(s);
+
+        if (v.name == "slot") {
+          slot = parseInt(v.value);
+          if (isNaN(slot)) throw "Bad slot number";
+          players[slot] = {
+            slot: 0,
+            uid: "0",
+            number: "0",
+            bike: "pw50",
+            name: "",
+            times: []
+          };
+          players[slot]["slot"] = slot;
+        } else players[slot][v.name] = v.value;
+      }
+    } else if (stage == readtimes) {
+      t = s.split(" ");
+
+      if (t.length != 3) throw "Bad time";
+
+      slot = parseInt(t[0]);
+      timeidx = parseInt(t[1]);
+      time = parseInt(t[2]);
+
+      if (isNaN(slot) || isNaN(timeidx) || isNaN(time)) throw "Bad time";
+
+      players[slot].times[timeidx] = time;
+    }
+  }
+
+  for (t in requiredints) {
+    t = requiredints[t];
+
+    if (!(t in raceinfo)) throw "Missing required variable '" + t + "'";
+
+    raceinfo[t] = parseInt(raceinfo[t]);
+
+    if (isNaN(raceinfo[t])) throw "'" + t + "' is not a valid integer";
+  }
+
+  raceinfo.players = players;
+
+  update_running_order(raceinfo);
+
+  return raceinfo;
+}
